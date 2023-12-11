3e66e197a82b50b050f10ec5451dc763fcb4005a
Alexander Wessels <alexwessels@yahoo.de>
2023-12-11 23:03:40 +0100

    simpleGetTable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,32 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  users: any[] = [];
+  error: string = '';
+  posts: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getUsers().subscribe((data) => {
+      this.posts = data;
+      console.log('Received data:', this.posts);
+    });
+  }
+
+  getData() {
+    this.dataService.getUsers().subscribe(
+      (data: any[]) => {
+        this.users = data;
+        this.error = ''; // Clear any previous error
+      },
+      (error) => {
+        this.error = 'An error occurred while fetching data.';
+        console.error(error);
+      }
+    );
+  }
+
   title = 'AngularWebClient';
 }
