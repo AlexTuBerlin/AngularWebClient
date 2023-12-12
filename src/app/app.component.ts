7c4a2163982df7076ef1279cb2a54afcf30b852e
Alexander Wessels <alexwessels@yahoo.de>
2023-12-12 22:41:58 +0100

    Added Post and Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,40 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DataService } from './data.service';
+import { FormsModule } from '@angular/forms';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  title = 'AngularWebClient';
   users: any[] = [];
-  error: string = '';
-  posts: any[] = [];
+  newUser: User = {name:'',vorname:'',email:''};
+  errorMessage: string = '';
+  userNameInput: string = '';
+  sub: any;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.dataService.getUsers().subscribe((data) => {
-      this.posts = data;
-      console.log('Received data:', this.posts);
+    this.getUsers()
+  }
+
+  getUsers() {
+    this.sub = this.dataService.getUsers().subscribe({
+      next: users => {
+        this.users = users;
+      },
+      error: err => this.errorMessage = err.message
     });
   }
 
-  getData() {
-    this.dataService.getUsers().subscribe(
-      (data: any[]) => {
-        this.users = data;
-        this.error = ''; // Clear any previous error
+  getUsersByName(user_name :string){
+    this.sub = this.dataService.getUsersByName(user_name).subscribe({
+      next: users => {
+        this.users = users;
       },
-      (error) => {
-        this.error = 'An error occurred while fetching data.';
-        console.error(error);
-      }
-    );
+      error: err => this.errorMessage = err.message
+    });
   }
 
-  title = 'AngularWebClient';
+  addUser(user: User){
+    this.sub = this.dataService.addUser(user).subscribe({
+      next: users => {
+        this.users = users;    
+        this.getUsers()
+      },
+      error: err => this.errorMessage = err.message
+    });
+  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,16 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private apiUrl = 'http://localhost:9000/api'; // Example API URL
-
+  private apiUrl = 'http://localhost:9000/api';
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/users`);
+  }
+
+  getUsersByName(user_name: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/users?name=${user_name}`);
+  }
+
+  addUser(newUser: User): Observable<any> {  
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const httpOptions = {
+      headers: headers
+    };
+    return this.http.post<any>(`${this.apiUrl}/users`, newUser, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+        )
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    if (err.error instanceof ErrorEvent) {
+      console.error('An error occurred:', err.error.message);
+    } else {
+      console.error(`Backend returned code ${err.status}, body was: ${err.error}`);
+    }
+    return throwError(() => err);
   }
 }
